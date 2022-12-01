702ba0ab17ab76a59dfc8730254d6ec80975d949
eugenewong <junyao5071@gmail.com>
2022-12-01 14:52:27 +0800

    initial commit

---
+++ exportRTGraph.py
@@ -0,0 +1,55 @@
+import smtplib
+import imghdr
+import os, shutil
+
+from email.message import EmailMessage
+
+def deleteAllFiles(folderPath):
+    for file in os.listdir(folderPath):
+        # Grab only png files
+        if not file.endswith(".php") and file.endswith(".py"):
+            file_path = os.path.join(folderPath, file)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+Sender_Email = "eugenewong@idgs.my"
+Reciever_Email = "eugenewong@ktj.edu.my"
+Bcc_Email = "tohseng@idgs.my"
+Password = "gqqnimxdpusfedzo"
+
+newMessage = EmailMessage()                         
+newMessage['Subject'] = "Cacti - Export Realtime Graph" 
+newMessage['From'] = Sender_Email                   
+newMessage['To'] = Reciever_Email
+newMessage['Bcc'] = Bcc_Email
+newMessage.set_content('Realtime graph report') 
+
+imagePath = "/home/netmon.monash.edu.my/public_html/cacti/cache/realtime/"
+imagesDir = os.listdir(imagePath)
+images = []
+
+for image in imagesDir:
+    if image.endswith(".png"):
+        images.append(image)
+
+for image in images:
+    # Open image file in binary mode
+    with open(image, "rb") as attachment:
+        image_data = attachment.read()
+        image_type = imghdr.what(attachment.name)
+        image_name = attachment.name
+    newMessage.add_attachment(image_data, maintype='image', subtype=image_type, filename=image_name)
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    
+    smtp.login(Sender_Email, Password)              
+    smtp.send_message(newMessage)
+
+# Remove graph imgages after email to receipient.
+deleteAllFiles(imagePath)
+
