325eecc1a279634c4dd71606613779a16723b33e
eugenewong <junyao5071@gmail.com>
2022-12-01 21:33:26 +0800

    test file

---
+++ test.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+# Cacti Credential
+username = "admin"
+password = "Monash123!!"
+
+chrome_options = Options()
+chrome_options.binary_location = '/usr/bin/google-chrome'
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument("--start-maximized")
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
+# Initialize the chrome driver
+driver = webdriver.Chrome(executable_path='/usr/lib/chromedriver', chrome_options=chrome_options)
+
+# Get the cacti login page
+driver.get("https://10.158.65.227/cacti/index.php")
+
+print(driver.find_element('xpth', '//*[@id="login"]/div[2]/table/tbody/tr[1]/td[1]/label'))
