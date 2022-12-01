0c50b734ac54450a150e99e6be59f7e0bb7e0a82
eugenewong <junyao5071@gmail.com>
2022-12-01 21:49:43 +0800

    test

--- test.py
+++ test.py
@@ -18,6 +18,6 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(executable_path='/usr/lib/chromedriver', chrome_options=chrome_options)
 
 # Get the cacti login page
-driver.get("https://10.158.65.227/cacti")
+driver.get("https://10.158.65.227:2020/cacti")
 
 print(driver.find_element('xpath', '/html/body/div[2]/div[2]').text)
