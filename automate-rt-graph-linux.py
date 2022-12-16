c2371ab33698334c180782cf363c1ff6270722c0
eugenewong <junyao5071@gmail.com>
2022-12-16 11:07:17 +0800

    commit

--- automate-rt-graph-linux.py
+++ automate-rt-graph-linux.py
@@ -55,6 +55,10 @@ WebDriverWait(driver, 10).until(
 
 # Click on Inter-Branch
 driver.find_element(By.ID, 'tbranch-6_anchor').click()
+# Delay for login to finished
+WebDriverWait(driver, 10).until(
+    EC.presence_of_all_elements_located((By.ID, 'graph_523_realtime'))
+)
 # Realtime Graph for all graphs
 # NOTE: GRAPH WILL NOT WORK IF THE TEMPLATE ID IS CHANGED
 driver.find_element('xpath','//*[@id="graph_523_realtime"]').click()
@@ -63,6 +67,10 @@ driver.find_element('xpath','//*[@id="graph_255_realtime"]').click()
 
 #Click on Core Infrastructure
 driver.find_element(By.ID, 'tbranch-5_anchor').click()
+# Delay for login to finished
+WebDriverWait(driver, 10).until(
+    EC.presence_of_all_elements_located((By.ID, 'graph_514_realtime'))
+)
 # Realtime Graph for all graphs
 driver.find_element('xpath', '//*[@id="graph_514_realtime"]').click()
 driver.find_element('xpath', '//*[@id="graph_512_realtime"]').click()
