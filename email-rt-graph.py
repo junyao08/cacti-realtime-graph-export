db787ccc05409ee23956961d8fd86ba45ad3ed14
eugenewong <junyao5071@gmail.com>
2022-12-19 10:08:08 +0800

    email debug

--- email-rt-graph.py
+++ email-rt-graph.py
@@ -78,6 +78,7 @@ try:
     print('Sending email...')
     #context = ssl.create_default_context()
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.set_debuglevel(1)
         server.login(email_from, password)
         server.sendmail(email_from, email_to, email_string)
     #deleteAllFiles(imagePath) 
