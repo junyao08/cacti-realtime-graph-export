fb88e5581c261a90c586e1bc6ad835889df4a538
eugenewong <junyao5071@gmail.com>
2022-12-21 16:06:05 +0800

    commit

--- email-rt-graph.py
+++ email-rt-graph.py
@@ -95,7 +95,7 @@ print('Convert it as a string')
 try:
     print('Sending email...')
     # context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("localhost", 465) as server:
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
         server.ehlo()
         server.set_debuglevel(1)
         server.login(email_from, password)
