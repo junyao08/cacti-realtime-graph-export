d9b7cdf5c682e544016aec9060d095a31329aeee
junya08 <junyao5071@gmail.com>
2022-12-22 17:37:07 +0800

    commit

--- remove_old_rt_graph.py
+++ remove_old_rt_graph.py
@@ -1,20 +1,5 @@
 import os, shutil
-import logging
 
-
-# Create a custom logger
-logger = logging.getLogger(__name__)
-
-# Create handlers
-f_handler = logging.FileHandler('deleted-files.log')
-f_handler.setLevel(logging.ERROR)
-
-# Create formatters and add it to handlers
-f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-f_handler.setFormatter(f_format)
-
-# Add handlers to the logger
-logger.addHandler(f_handler)
 
 # Function to delete realtime graph that has been sent.
 def deleteAllFiles(folderPath):
@@ -27,10 +12,8 @@ def deleteAllFiles(folderPath):
                     os.unlink(file_path)
                 elif os.path.isdir(file_path):
                     shutil.rmtree(file_path)
-                logger.error('File removed: ', file)
             except Exception as e:
-                logger.error('Failed to delete %s. Reason: %s' % (file_path, e))
-
+                print("Error while deleting files")
 
 imagePath = '.'
 deleteAllFiles(imagePath)
