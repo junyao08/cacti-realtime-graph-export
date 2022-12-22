4ec624cc16674391408dd33f739c5daa9a186e4d
eugenewong <junyao5071@gmail.com>
2022-12-22 10:21:36 +0800

    commit

--- test.py
+++ test.py
@@ -44,7 +44,7 @@ def attach_file_to_email(email_message, filename):
             )
             # Attach the file to the message
             email_message.attach(file_attachment)
-            logger.error('File name: ', filename)
+            print('File name: ', filename)
         except Exception as e:
             logger.error('Error sending png: ', e)
 
@@ -68,12 +68,19 @@ def deleteAllFiles(folderPath):
 # Set up the email addresses and password. Please replace below with your email address and password
 email_from = 'eugenewong@idgs.my'
 email_to = 'eugenewong@idgs.my'
+#cc = 'james.chia@monash.edu'
+#bcc = ['tohseng@idgs.my', 'eugenewong@idgs.my']
+
+smtp_server = 'localhost'
+smtp_port = 25
 
 # Create a MIMEMultipart class, and set up the From, To, Subject fields
 email_message = MIMEMultipart()
 email_message['From'] = email_from
 email_message['To'] = email_to
 email_message['Subject'] = f'Netmon - Realtime Graph'
+email_message['Cc'] = cc
+email_message['Bcc'] = ', '.join(bcc)
 
 # Attach the html doc defined earlier, as a MIMEText html content type to the MIME message
 email_message.attach(MIMEText(html, "html"))
@@ -91,23 +98,17 @@ email_string = email_message.as_string()
 print('Convert it as a string')
 
 # Connect to the Gmail SMTP server and Send Email
-# try:
-#     print('Sending email...')
-#     # context = ssl.create_default_context()
-#     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-#         server.ehlo()
-#         server.set_debuglevel(1)
-#         server.login(email_from, password)
-#         server.sendmail(email_from, email_to, email_string)
-#         server.close()
-#     deleteAllFiles(imagePath) 
-#     print('Email is sent')
-# except Exception as e:
-#     logger.error("Sending Error:", e)
-
 try:
-    with smtplib.SMTP('localhost', 25) as server:
+    print('Sending email...')
+    # context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, smtp_port) as server:
+        server.ehlo()
         server.set_debuglevel(1)
-        server.send_message(email_from, email_to, email_string)
+        server.sendmail(email_from, email_to, email_string)
+        server.close()
+    deleteAllFiles(imagePath) 
+    print('Email is sent')
 except Exception as e:
-    logger.error("Email errors: ", e)
+    logger.error("Sending Error:", e)
+
+
