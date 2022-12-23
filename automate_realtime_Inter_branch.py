0a0924b939f58497b016765f2c78f25183802f04
junya08 <junyao5071@gmail.com>
2022-12-23 11:38:53 +0800

    commit

--- automate_realtime_Inter_branch.py
+++ automate_realtime_Inter_branch.py
@@ -6,9 +6,6 @@ from selenium.webdriver.common.by import By
 import time
 import os
 
-# Stop while email are sending and deleting the old files
-time.sleep(40)
-
 # Cacti Credential
 username = "admin"
 password = "Monash123!!"
