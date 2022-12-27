da04fdcc61b846e72fae099693e97144fcd03d6d
junya08 <junyao5071@gmail.com>
2022-12-27 16:51:02 +0800

    commit

--- archive_realtime_files.py
+++ archive_realtime_files.py
@@ -33,7 +33,7 @@ def zip_rename_file(imageDir):
   
     now = datetime.datetime.now()
     timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
-    zip_filename = os.path.join('/var/www/html/ftp_file', '{}.zip'.format(timestamp)) 
+    zip_filename = os.path.join('/var/www/html/ftp_file/', '{}.zip'.format(timestamp)) 
 
     # writing files to a zipfile
     with ZipFile(zip_filename,'w') as zip:
