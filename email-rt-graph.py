c0dc57a28d46cb7078840ae6994eb2a00001294e
junya08 <junyao5071@gmail.com>
2022-12-27 14:47:55 +0800

    commit

--- email-rt-graph.py
+++ email-rt-graph.py
@@ -55,23 +55,23 @@ def rename_graph_name(folderpath, filename):
     image_path = os.path.join(folderpath, filename)
     if filename.endswith('.png') or filename.endswith('.svg'):
         if filename.endswith('514.png'):
-            os.rename(image_path, 'musc-cx2-sw1-time-500m-dia.png')
+            os.rename(image_path, folderpath + 'musc-cx2-sw1-time-500m-dia.png')
         elif filename.endswith('512.png'):
-            os.rename(image_path, 'musc-aims-sw1-maxis-500m-dia.png')
+            os.rename(image_path, folderpath + 'musc-aims-sw1-maxis-500m-dia.png')
         elif filename.endswith('515.png'):
-            os.rename(image_path, 'musc-cx2-sw1-myix-1g-cx2.png')
+            os.rename(image_path, folderpath + 'musc-cx2-sw1-myix-1g-cx2.png')
         elif filename.endswith('513.png'):
-            os.rename(image_path, 'musc-aims-sw-myix-1g-aims.png')
+            os.rename(image_path, folderpath +'musc-aims-sw-myix-1g-aims.png')
         elif filename.endswith('508.png'):
-            os.rename(image_path, 'musc-9-5-gw1-myren-300m-ppl.png')
+            os.rename(image_path, folderpath +'musc-9-5-gw1-myren-300m-ppl.png')
         elif filename.endswith('507.png'):
-            os.rename(image_path, 'musc-9-5-gw1-1g-pll-to-cx2.png')
+            os.rename(image_path, folderpath + 'musc-9-5-gw1-1g-pll-to-cx2.png')
         elif filename.endswith('510.png'):
-            os.rename(image_path, 'musc-2-0-gw1-1g-pll-to-aims.png')
+            os.rename(image_path, folderpath + 'musc-2-0-gw1-1g-pll-to-aims.png')
         elif filename.endswith('523.png'):
-            os.rename(image_path, 'musc-vss-core-csjb-300m-pll.png')
+            os.rename(image_path, folderpath + 'musc-vss-core-csjb-300m-pll.png')
         elif filename.endswith('1281.png'):
-            os.rename(image_path, 'musc-9-5-bds1-smp-100m-pll.png')
+            os.rename(image_path, folderpath + 'musc-9-5-bds1-smp-100m-pll.png')
 
 # Function to attach files as MIMEApplication to the email
 def attach_file_to_email(email_message, filename):
